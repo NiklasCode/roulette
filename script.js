2a7f64a26ac72acde28db0e282acb5e43098f268
Niklas <Niklas.lundberg@live.se>
2022-09-09 13:55:19 +0200

    added files

---
+++ script.js
@@ -0,0 +1,22 @@
+
+var random = Math.floor(Math.random() *37);
+console.log(random);
+
+
+var spel1 
+
+var spel2
+
+function knapp() {
+   
+}
+
+function spela(){
+    
+    document.getElementById("spel1").innerHTML = spel1;
+
+    document.getElementById("spel2").innerHTML = spel2
+
+    document.getElementById("nummerxsxs").innerHTML = Math.floor(Math.random() *37);
+}
+
